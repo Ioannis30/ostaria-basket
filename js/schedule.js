018fb10c3b4cc57644dd085de64b892caa97e5ca
Ioannis30 <cdisavoiacarignano@gmail.com>
2026-04-19 03:45:57 +0200

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -1,10 +1,15 @@
 ```javascript
-fetch("data/schedule.json")
-  .then(r => r.json())
-  .then(data => {
-    const div = document.getElementById("games");
-    data.forEach(g => {
-      div.innerHTML += `<p>${g.home} vs ${g.away}</p>`;
-    });
-  });
+fetch("data/results.json")
+.then(r=>r.json())
+.then(data=>{
+ const div=document.getElementById("games");
+ data.forEach(g=>{
+  div.innerHTML+=`
+   <div class="match-card">
+     <div class="team">${g.home}</div>
+     <div class="score">${g.homeScore} - ${g.awayScore}</div>
+     <div class="team">${g.away}</div>
+   </div>`;
+ });
+});
 ```
