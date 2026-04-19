f836629a4eec7296b9fcb3e5cf1477fc842a64eb
Ioannis30 <cdisavoiacarignano@gmail.com>
2026-04-19 03:45:19 +0200

    Update standings.js

--- js/standings.js
+++ js/standings.js
@@ -1,62 +1,33 @@
 ```javascript
 Promise.all([
-  fetch("data/results.json").then(r => r.json()),
-  fetch("data/teams.json").then(r => r.json())
-])
-.then(([results, teams]) => {
+ fetch("data/results.json").then(r=>r.json()),
+ fetch("data/teams.json").then(r=>r.json())
+]).then(([results,teams])=>{
 
-  let table = {};
+let table={};
+teams.forEach(t=>table[t.name]={w:0,l:0});
 
-  teams.forEach(t => {
-    table[t.name] = {
-      w:0,
-      l:0,
-      conference: t.conference,
-      division: t.division
-    };
-  });
+results.forEach(g=>{
+ if(g.homeScore>g.awayScore){table[g.home].w++;table[g.away].l++;}
+ else{table[g.away].w++;table[g.home].l++;}
+});
 
-  results.forEach(g => {
-    if (g.homeScore > g.awayScore) {
-      table[g.home].w++;
-      table[g.away].l++;
-    } else {
-      table[g.away].w++;
-      table[g.home].l++;
-    }
-  });
+let ranking=Object.entries(table).map(([team,s])=>{
+ let pct=(s.w/(s.w+s.l)||0).toFixed(3);
+ return {team,...s,pct};
+}).sort((a,b)=>b.pct-a.pct);
 
-  let ranking = Object.entries(table).map(([team, stats]) => {
-    let games = stats.w + stats.l;
-    let pct = games ? (stats.w / games).toFixed(3) : 0;
-    return {team, ...stats, pct};
-  });
+const el=document.getElementById("table");
+el.innerHTML=`<tr><th>#</th><th>Team</th><th>W</th><th>L</th><th>%</th></tr>`;
 
-  // Trier par conférence puis %
-  ranking.sort((a,b) => {
-    if (a.conference !== b.conference) {
-      return a.conference.localeCompare(b.conference);
-    }
-    return b.pct - a.pct;
-  });
-
-  const el = document.getElementById("table");
-
-  let currentConf = "";
-
-  ranking.forEach(t => {
-
-    if (t.conference !== currentConf) {
-      currentConf = t.conference;
-      el.innerHTML += `<tr><td colspan="5"><b>${currentConf} Conference</b></td></tr>`;
-    }
-
-    el.innerHTML += `<tr>
-      <td>${t.team}</td>
-      <td>${t.w}</td>
-      <td>${t.l}</td>
-      <td>${t.pct}</td>
-    </tr>`;
-  });
+ranking.forEach((t,i)=>{
+ el.innerHTML+=`<tr class="row">
+  <td>${i+1}</td>
+  <td>${t.team} ${i<4?'<span class="badge">PO</span>':''}</td>
+  <td>${t.w}</td>
+  <td>${t.l}</td>
+  <td>${t.pct}</td>
+ </tr>`;
+});
 });
 ```
